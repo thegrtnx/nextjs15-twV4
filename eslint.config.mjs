70cdcf24f825480e4c5b120a62d1a641c53b1ee6
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2025-09-26 07:18:37 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ["next/core-web-vitals", "next/typescript", "next", "prettier"],
+		rules: {
+			"react/no-unescaped-entities": "off",
+			"@next/next/no-page-custom-font": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/unused-vars": "off",
+		},
+	}),
 ];
 
-export default eslintConfig;
+export default eslintConfi;
